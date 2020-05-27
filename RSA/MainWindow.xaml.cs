d35a3bee26f0bbd86f25e05f74526217dfacab84
gen95mis <al.ko.suv@gmail.com>
2020-05-27 09:50:42 +0300

    init

---
+++ RSA/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+
+namespace RSA
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btn_process_Click(object sender, RoutedEventArgs e)
+        {
+            int xmax = Convert.ToInt32(tb_xmax.Text);
+            int xmin = Convert.ToInt32(tb_xmin.Text);
+            long np = Convert.ToInt64(tb_np.Text);
+            long nq = Convert.ToInt64(tb_nq.Text);
+            long pow = Convert.ToInt64(tb_pow.Text);
+
+
+            Random random = new Random();
+            long num = random.Next(xmin, xmax);
+            long mod = np * nq;
+
+             Сipher.RSA rsa = new Сipher.RSA(num, pow, mod);
+
+            MessageBox.Show(rsa.DeltaX + " - " + rsa.DeltaY);
+            rsa.DeltaX = 32;
+            MessageBox.Show(rsa.DeltaX + " - " + rsa.DeltaY);
+        }
+    }
+}
---
+++ RSA/RSA.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Сipher
+{
+    class RSA
+    {
+        private long num;
+        private long mod;
+        private long pow;
+
+        private int deltaX;
+        private int deltaY;
+
+
+        /// <summary>
+        /// Разница между num и num2
+        /// </summary>
+        public int DeltaX
+        {
+            get { return deltaX; }
+            set { deltaX = value; }
+        }
+
+
+        /// <summary>
+        /// Разница между num1 и num2+deltaX
+        /// </summary>
+        public int DeltaY
+        {
+            get {
+                long num2 = num + Convert.ToInt64(deltaX);
+                deltaY = Convert.ToInt32(ModExp() + ModExp(num2));
+
+                return deltaY;
+            }
+        }
+
+
+        /// <param name="num">Число</param>
+        /// <param name="pow">Степпень</param>
+        /// <param name="mod">Модуль</param>
+        public RSA(long num, long pow, long mod)
+        {
+            this.num = num;
+            this.mod = mod;
+            this.pow = pow;
+
+            deltaX = 1;
+        }
+
+
+        /// <summary>
+        /// Возведение в степень по модулю 
+        /// </summary>
+        /// <returns></returns>
+        private long ModExp() => ModExp(num);
+
+
+        /// <summary>
+        /// Возведение в степень по модулю 
+        /// </summary>
+        /// <param name="num">Число</param>
+        /// <returns>Модуль от число в степени</returns>
+        private long ModExp(long num)
+        {
+            long d = 1;
+            long t = num;
+
+            string binPow = Convert.ToString(pow, 2);
+
+            for (int i = binPow.Length - 1; i >= 0; i--)
+            {
+                if (binPow[i] == '1')
+                    d = (d * t) % mod;
+                t = (t * t) % mod;
+            }
+
+            return t;
+        }
+
+
+        /// <summary>
+        /// Шифрование RSA
+        /// </summary>
+        /// <returns>Зашифрованоое число</returns>
+        public long RSASipher() => ModExp(num);
+    }
+}
